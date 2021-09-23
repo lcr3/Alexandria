9f1771191f433ab55b752db75e80dceebf5a343b
lcr <llcoolryo@gmail.com>
2021-09-23 11:58:19 +0900

    [App] First commit

---
+++ Alexandria/AlexandriaApp.swift
@@ -0,0 +1,18 @@
+//
+//  AlexandriaApp.swift
+//  Alexandria
+//
+//  Created by lcr on 2021/09/23.
+//  
+//
+
+import SwiftUI
+
+@main
+struct AlexandriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
