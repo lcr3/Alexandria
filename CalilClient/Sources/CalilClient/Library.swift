f94eb1d1a4da485e680ea69fa95f2a69917edc4d
lcr <llcoolryo@gmail.com>
2021-09-25 23:59:59 +0900

    [App] Fix Library entity

--- CalilClient/Sources/CalilClient/Library.swift
+++ CalilClient/Sources/CalilClient/Library.swift
@@ -1,20 +1,25 @@
 import APIKit
 import Foundation
+import CloudKit
+import SwiftUI
 
-public struct Library: Decodable {
-    let address: String
-    let pref: String
-    let libid: String
-    let systemid: String
-    let systemName: String
-    let category: String
+public struct Library: Decodable, Identifiable {
+    public var id: String {
+        return systemId
+    }
+    public let address: String
+    public let pref: String
+    public let libid: String
+    public let systemId: String
+    public let systemName: String
+    public let category: String
 
     public init(object: Any) throws {
         guard let dictionary = object as? [String: Any],
               let address = dictionary["address"] as? String,
               let pref = dictionary["pref"] as? String,
               let libid = dictionary["libid"] as? String,
-              let systemid = dictionary["systemid"] as? String,
+              let systemId = dictionary["systemid"] as? String,
               let systemName = dictionary["systemname"] as? String,
               let category = dictionary["category"] as? String else {
                   throw ResponseError.unexpectedObject(object)
@@ -22,7 +27,16 @@ public struct Library: Decodable {
         self.address = address
         self.pref = pref
         self.libid = libid
-        self.systemid = systemid
+        self.systemId = systemId
+        self.systemName = systemName
+        self.category = category
+    }
+
+    public init(address: String = "", pref: String = "", libid: String = "", systemId: String = "", systemName: String = "", category: String = "") {
+        self.address = address
+        self.pref = pref
+        self.libid = libid
+        self.systemId = systemId
         self.systemName = systemName
         self.category = category
     }
