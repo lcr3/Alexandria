02aa1a4e732918d486eb8fcc9e786a38da4f6415
lcr <llcoolryo@gmail.com>
2021-10-13 13:12:59 +0900

    [Client] Add init

--- StorageClient/Sources/StorageClient/StorageClient.swift
+++ StorageClient/Sources/StorageClient/StorageClient.swift
@@ -3,6 +3,11 @@ import Foundation
 public struct Location {
     let latitude: Double
     let longitude: Double
+
+    public init(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
 }
 
 public protocol StorageClientProtocol {
