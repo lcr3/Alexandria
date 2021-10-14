923ea9bc65a9705ac71076b76eb4f74e42407e2a
lcr <llcoolryo@gmail.com>
2021-10-14 14:57:29 +0900

    [Tool] Add license-plist

---
+++ Tools/Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.4.0
+import PackageDescription
+
+let package = Package(
+    name: "Tools",
+    platforms: [.iOS("15.0")],
+    products: [
+    ],
+    dependencies: [
+        .package(url: "https://github.com/mono0926/LicensePlist", from: "3.14.4"),
+//          .package(url: "https://github.com/apple/swift-format", from: "0.48.5")
+    ],
+    targets: [
+    ]
+)
