50e53b22ecf6d5065bbb55221b1d8bda5f02835d
lcr <llcoolryo@gmail.com>
2021-11-07 09:17:21 +0900

    [App] Fix Test scheme

--- AlexandriaUITests/AlexandriaUITests.swift
+++ AlexandriaUITests/AlexandriaUITests.swift
@@ -3,13 +3,12 @@
 //  AlexandriaUITests
 //
 //  Created by lcr on 2021/09/23.
-//  
+//
 //
 
 import XCTest
 
 class AlexandriaUITests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
@@ -21,14 +20,5 @@ class AlexandriaUITests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 }
